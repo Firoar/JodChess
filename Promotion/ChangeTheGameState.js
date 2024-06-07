01d41f924dc73cedf67933d9569950c7d8701d00
chiru <149101313+Firoar@users.noreply.github.com>
2024-06-07 20:53:54 +0530

    Update ChangeTheGameState.js

--- Promotion/ChangeTheGameState.js
+++ Promotion/ChangeTheGameState.js
@@ -1,55 +1,61 @@
-
 import { DICTIONARY } from "../utilities/getPositionOfKing.js";
-
 const ChangeTheGameState = (game_state, arr) => {
   let [i, j] = arr[1];
   let color = arr[2];
   let piece = arr[3];
 
-  let Current_Game_Position = JSON.parse(
+  let Current_Game_Psoition = JSON.parse(
     JSON.stringify(game_state[game_state.length - 1])
   );
   let str = String(i) + "," + String(j);
   let box = document.querySelector(".box_" + DICTIONARY[str]);
   let IMAGE = document.createElement("img");
 
-  IMAGE.addEventListener("load", () => {
-    // Once the image is loaded, clear the innerHTML of the box and then append the image
-    box.innerHTML = ""; // Clear existing content
-    box.appendChild(IMAGE); // Append the image
-  });
-
   if (piece === "knight") {
-    Current_Game_Position[i][j] = color === "white" ? "wN" : "bN";
-    IMAGE.src =
-      color === "white"
-        ? "../assets/white-knight.png"
-        : "../assets/black-knight.png";
-    IMAGE.classList.add(color, color + "-knight", "img");
-  } else if (piece === "bishop") {
-    Current_Game_Position[i][j] = color === "white" ? "wB" : "bB";
-    IMAGE.src =
-      color === "white"
-        ? "../assets/white-bishop.png"
-        : "../assets/black-bishop.png";
-    IMAGE.classList.add(color, color + "-bishop", "img");
-  } else if (piece === "rook") {
-    Current_Game_Position[i][j] = color === "white" ? "wR" : "bR";
-    IMAGE.src =
-      color === "white"
-        ? "../assets/white-rook.png"
-        : "../assets/black-rook.png";
-    IMAGE.classList.add(color, color + "-rook", "moved", "img");
-  } else if (piece === "queen") {
-    Current_Game_Position[i][j] = color === "white" ? "wQ" : "bQ";
-    IMAGE.src =
-      color === "white"
-        ? "../assets/white-queen.png"
-        : "../assets/black-queen.png";
-    IMAGE.classList.add(color, color + "-queen", "img");
-  }
+    Current_Game_Psoition[i][j] = color === "white" ? "wN" : "bN";
 
-  return Current_Game_Position;
+    if (color === "white") {
+      IMAGE.src = `../assets/white-knight.png`;
+    } else {
+      IMAGE.src = `../assets/black-knight.png`;
+    }
+
+    IMAGE.classList.add(`${color}`, `${color}-knight`, `img`);
+    //   IMAGE.alt
+  } else if (piece === "bishop") {
+    Current_Game_Psoition[i][j] = color === "white" ? "wB" : "bB";
+    if (color === "white") {
+      IMAGE.src = `../assets/white-bishop.png`;
+    } else {
+      IMAGE.src = `../assets/black-bishop.png`;
+    }
+
+    IMAGE.classList.add(`${color}`, `${color}-bishop`, `img`);
+    // box.innerHTML = IMAGE;
+  } else if (piece === "rook") {
+    Current_Game_Psoition[i][j] = color === "white" ? "wR" : "bR";
+
+    if (color === "white") {
+      IMAGE.src = `../assets/white-rook.png`;
+    } else {
+      IMAGE.src = `../assets/black-rook.png`;
+    }
+    IMAGE.classList.add(`${color}`, `${color}-rook`, `moved`, `img`);
+    // box.innerHTML = IMAGE;
+  } else if (piece === "queen") {
+    Current_Game_Psoition[i][j] = color === "white" ? "wQ" : "bQ";
+
+    if (color === "white") {
+      IMAGE.src = `../assets/white-queen.png`;
+    } else {
+      IMAGE.src = `../assets/black-queen.png`;
+    }
+    IMAGE.classList.add(`${color}`, `${color}-queen`, `img`);
+    // box.innerHTML = IMAGE;
+  }
+  box.appendChild(IMAGE.cloneNode(true));
+
+  return Current_Game_Psoition;
 };
 
 export default ChangeTheGameState;
